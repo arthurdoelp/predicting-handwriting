416916a4fec2c7b8e54d8bd7068bc52638a9ac2b
arthurdoelp <arthurdoelp@gmail.com>
2021-02-03 13:29:43 -0500

    Added env for database_url

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
-const Sequelize = require("sequelize");
-const db = {};
-    const sequelize = new Sequelize("predicting_handwriting", "arthurdoelp", "", {
-        host: 'localhost',
+if (process.env.DATABASE_URL) {
+    const Sequelize = require("sequelize");
+    const db = {};
+    const sequelize = new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres',
+        protocol: 'postgres',
 
         pool: {
             max: 5,
@@ -12,9 +13,30 @@ const db = {};
         }
 
     });
-    console.log("PostgreSQL Connected!");
-
+    console.log("PostgreSQL Connected on heroku!");
     db.sequelize = sequelize;
     db.Sequelize = Sequelize;
 
-module.exports = db;
+    module.exports = db;
+} else {
+    const Sequelize = require("sequelize");
+    const db = {};
+        const sequelize = new Sequelize("predicting_handwriting", "arthurdoelp", "", {
+            host: 'localhost',
+            dialect: 'postgres',
+    
+            pool: {
+                max: 5,
+                min: 0,
+                acquire: 30000,
+                idle: 10000
+            }
+    
+        });
+        console.log("PostgreSQL Connected!");
+    
+        db.sequelize = sequelize;
+        db.Sequelize = Sequelize;
+    
+    module.exports = db;
+}
